20a0ba6d356182ee960fedf3dc9f885eec86fe41
ZiLiangDang <2013841882@qq.com>
2026-05-16 10:34:32 +0800

    gloabl_center编写

---
+++ src/planning/src/global_planner/global_planner_base.h
@@ -0,0 +1,14 @@
+#ifndef GLOBAL_PLANNER_BASE_H_
+#define GLOBAL_PLANNER_BASE_H_
+
+#include "rclcpp/rclcpp.hpp"
+#include "config_reader.h"
+
+namespace Planning
+{
+    class GlobalPlannerBase // 全局路径规划器基类
+    {
+    public:
+    };
+} // namespace Planning
+#endif // GLOBAL_PLANNER_BASE_H_
---
+++ src/planning/src/global_planner/global_planner_normal/global_planner_normal.h
@@ -0,0 +1,14 @@
+#ifndef GLOBAL_PLANNER_NOMAL_H_
+#define GLOBAL_PLANNER_NOMAL_H_
+
+#include "global_planner_base.h"
+
+namespace Planning
+{
+class GlobalPlannerNormal : public GlobalPlannerBase //普通全局路径规划器
+{
+    public:
+        GlobalPlannerNormal();
+    };
+}  // namespace /* namespace_name */
+#endif  // GLOBAL_PLANNER_NOMAL_H_
