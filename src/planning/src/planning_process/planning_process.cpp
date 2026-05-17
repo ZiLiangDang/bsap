e72193d7f03e967da29b56c6f8ec3b5fc9186695
ZiLiangDang <2013841882@qq.com>
2026-05-17 12:17:11 +0800

    配置文件的读取

--- src/planning/src/planning_process/planning_process.cpp
+++ src/planning/src/planning_process/planning_process.cpp
@@ -2,13 +2,18 @@
 
 namespace Planning
 {
-     PlanningProcess::PlanningProcess(): Node("planning_node")//规划总流程
-        {
-            RCLCPP_INFO(this->get_logger(), "Planning_node  ceated");
-        }
+    PlanningProcess::PlanningProcess() : Node("planning_process") // 规划总流程
+    {
+        RCLCPP_INFO(this->get_logger(), "planning_process created");
 
-        bool PlanningProcess::process()//总流程
-        {
-            return true;
-        }
-}
+        // 读取配置文件
+        process_config_ = std::make_unique<ConfigReader>();
+        process_config_->read_planning_process_config();
+        auto obs_dis = process_config_->process().obs_dis_;
+    }
+
+    bool PlanningProcess::process() // 总流程
+    {
+        return true;
+    }
+} // namespace Planning
--- src/planning/src/planning_process/planning_process.h
+++ src/planning/src/planning_process/planning_process.h
@@ -24,6 +24,7 @@ namespace Planning
         PlanningProcess();
         bool process();//总流程
     private:
+        std::unique_ptr<ConfigReader> process_config_; //配置
     };
 }  // namespace Planning
 #endif  // PLANNING_PROCESS_H_
