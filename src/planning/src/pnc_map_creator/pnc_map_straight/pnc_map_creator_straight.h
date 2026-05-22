2f157bd646001b1a00982cd0e76c10ea78fc7c65
ZiLiangDang <2013841882@qq.com>
2026-05-22 11:31:40 +0800

    直道地图

--- src/planning/src/pnc_map_creator/pnc_map_straight/pnc_map_creator_straight.h
+++ src/planning/src/pnc_map_creator/pnc_map_straight/pnc_map_creator_straight.h
@@ -12,6 +12,8 @@ namespace Planning
         PNCMap create_pnc_map() override; // 创建地图接口
 
     private:
+        void init_pnc_map(); // 初始化地图
+        void draw_straight_x(const double &length,const double &plus_flag, const double &ratio = 1.0); // 绘制x方向的直道
     };
 } // namespace Planning
 #endif // PNC_MAP_CREATOR_STRAIGHT_H_
